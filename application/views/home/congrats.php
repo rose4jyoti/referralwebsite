62c1f23577a8348b8c6ae7b9b195894ccdb0af1b
jyoti <rose4jyoti@gmail.com>
2013-12-19 20:20:45 +0530

    congrats page modification

--- application/views/home/congrats.php
+++ application/views/home/congrats.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.'); ?>
 <?php echo Html::style('public/css/style.css'); ?>
 <?php echo Html::style('public/css/page7.css'); ?>
+
 <?php 
 foreach ($options as $temp) :
  if($temp['referralProgImageOrder']=='3'){
@@ -121,11 +122,22 @@ of your friends will have
 
   <div class="ss_out_new">
 							<h4 class="rr">
-							<input type="text" class="new_in_box" name="">
-                                <a href="#"><img style=" float: left;
-    margin: 0;
-    padding: 4px;" src="/dev2013/referral/public/image/search_but.png" alt="">
+							
+							 <?= Form::open('home/congrats'); ?>
+                              <?= Form::input('key','',  array('class'=>'new_in_box')); ?>
+							  <!---<input type="text" class="new_in_box" name="">---->
+							  <?php echo Form::hidden('formid', '1'); ?>
+							  <button style="border:none; background:none;height: 22px;width: 28px;" type="submit">
+							  <img style=" float: left;margin: 0; padding: 4px;" src="/dev2013/referral/public/image/search_but.png" alt="">
+							 </button>							  
+							<?= Form::close(); ?>
+							
+                               <!--- <a href="#">
+								<img style=" float: left; margin: 0; padding: 4px;" src="/dev2013/referral/public/image/search_but.png" alt="">
 								</a>
+								---->
+								
+								
 								
                              </h4>
 							</div>
@@ -133,7 +145,41 @@ of your friends will have
 <div class="song_over">
 
 <table class="songs_list">
-  <tbody><tr class="no_back">
+  <tbody>
+  
+  
+  <?php foreach ($contacts as $temp) : ?>					  
+   <tr class="no_back">
+    <td style="" width="30%" valign="middle" align="left"><b><?php echo $temp['name']; ?></b></td>
+    <td width="40%" valign="middle" align="center"><?php echo $temp['email']; ?> </td>
+      <td width="5%" valign="middle" align="center">
+	  
+	  <img alt="" src="/dev2013/referral/public/image/ok.jpg">	 
+	  
+	  </td>
+    <td width="10%" valign="middle" align="center">
+	<span>
+	<?php 
+	if($temp['sent_status']==1){
+	 echo 'Sent';
+	}
+	else{
+	  echo 'Not Invited';
+	}
+	
+
+	?>
+	</span></td>
+    <td width="15%">
+	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
+	</td>
+   </tr>					  
+
+ <?php endforeach; ?>
+  
+
+  <!---
+   <tr class="no_back">
     <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
     <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
       <td width="9%" valign="middle" align="center">
@@ -156,148 +202,11 @@ of your friends will have
 	
 	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
 	
-	
 	</td>
   </tr>
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/invii.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/invii.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/invii.png">
-	
-	</td>
-  </tr>
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-  
-  
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-   <tr class="no_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Sent</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
-  
-   <tr class="with_back">
-    <td width="17%" valign="middle" align="left"><b>Staphane</b></td>
-    <td width="35%" valign="middle" align="center">Staphane@gmail.com</td>
-      <td width="9%" valign="middle" align="center"><img alt="" src="/dev2013/referral/public/image/ok.jpg"></td>
-    <td width="14%" valign="middle" align="center"><span>Registered</span></td>
-    <td width="25%">
-	
-	<img class="r_image" alt="" src="/dev2013/referral/public/image/remind.png">	
-	
-	</td>
-  </tr>
+  ---->
+
+
   
 </tbody></table>
 
@@ -305,7 +214,19 @@ of your friends will have
 
 <h1 class="achieve"> increase your chances to WIN and share on social!</h1>
 <div class="link_url"><h3>Your unique referral link:</h3>
-<input type="text" name="" class="url">
+<input type="text" name="" value="abc" class="url" id="uniquerl">
+
+<!------Filling value in text box to unique referral link---->
+<script type="text/javascript">
+ //val=document.getElementById('uniquerl').value;
+  document.getElementById('uniquerl').value='<?php echo URL::base(); ?>front/<?php echo $uniquerl=uniqid(); ?>';
+  alert(val);
+  
+ //$("#uniquerl").attr ( "value" ,"nvsss" );
+ //document.getelementById('uniquerl')='<?php echo $uniquerl=uniqid(); ?>';
+</script>
+
+
 </div>
 <h1 class="achieve_social"> invite your social contacts to participate:</h1>
 
@@ -317,12 +238,6 @@ of your friends will have
 <a href="#">
 <img alt="" src="/dev2013/referral/public/image/in_i.jpg"></a>
 </div>
-
-
-
-
-
-
 
 
 
