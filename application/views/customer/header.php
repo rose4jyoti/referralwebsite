8697c9274c7c2ba904a4558f922467035d616af5
jyoti <rose4jyoti@gmail.com>
2014-02-17 20:09:45 +0530

    New file header and footer

---
+++ application/views/customer/header.php
@@ -0,0 +1,49 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<?php echo Html::style('public/css/style.css'); ?>
+
+<div class="header_out">
+      <div class="header_in">
+        <div class="logo">
+		<?php echo Html::image('public/image/dummy-logo.png', array('alt'=>''));  ?>
+		
+		
+		</div>
+        <span class="cus_name"> Welcome &nbsp;<?php echo $customername; ?></span> 
+        <!---ul class="main_nav">
+          <li><a href="#">Campaigns</a> </li>
+          <li> <a href="#">Account </a></li>
+          <li> <a href="#">Support </a></li>
+          <li><a href="#"> Logout </a></li>
+        </ul--->
+        
+        <div class="new_ul">
+          <ul id="menu">
+            <li><a href="<?php echo URL::base(); ?>/customer/campaign">Campaigns</a>
+			<ul>
+			 <?php foreach ($campaigns as $temp) : ?>
+              <li><a href="<?php echo URL::base(); ?>/customer/dashboard/<?php echo $temp['referralProgDetailsID']; ?>"><?php echo $temp['referralProgTitle']; ?></a></li>	
+			 <?php endforeach; ?>	
+				
+                <li><a href="<?php echo URL::base(); ?>/customer/start">Add new Campaigns</a></li> 
+              </ul>
+
+			</li>
+			
+		  <li><a href="#">Account</a>
+              <ul>
+                <li><a href="<?php echo URL::base(); ?>/customer/account">Account Setting</a></li>
+                <li><a href="<?php echo URL::base(); ?>/customer/billing">Billing information</a></li>
+               
+              </ul>
+            </li>
+            <li><a href="<?php echo URL::base(); ?>/customer/support">Support</a>
+            </li>
+			
+            <li><a href="<?php echo URL::base(); ?>/user/logout">Logout</a> </li>
+          </ul>
+        </div>
+      </div>
+  
+  </div>
+	
+	
