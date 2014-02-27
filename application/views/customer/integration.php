b7e76808c57fd102578cf06e35896d3112f0f7df
jyoti <rose4jyoti@gmail.com>
2014-02-27 13:40:39 +0530

    code of modification

--- application/views/customer/integration.php
+++ application/views/customer/integration.php
@@ -22,38 +22,8 @@
 
 <div class="wrapper_out">
   <div class="wrapper_in">
-    <div class="header_out">
-      <div class="header_in">
-        <div class="logo">
-		
-		<?php echo Html::image('public/image/dummy-logo.png', array('style'=>''));  ?>
-		
-		</div>
-         <span class="cus_name"> Welcome &nbsp;<?php echo $customername; ?></span> 
-        <div class="new_ul">
-          <ul id="menu">
-            <li><a href="#">Campaigns</a>
-			 <ul>
-			 <?php foreach ($campaigns as $temp) : ?>
-                  <li><a href="<?php echo URL::base(); ?>/customer/dashboard/<?php echo $temp['referralProgDetailsID']; ?>"><?php echo $temp['referralProgTitle']; ?></a></li>
-			 <?php endforeach; ?>	
-                <li><a href="<?php echo URL::base(); ?>/customer/start">Add new Campaigns</a></li> 
-              </ul>
-			</li>
-            <li><a href="#">Account</a>
-              <ul>
-                <li><a href="<?php echo URL::base(); ?>/customer/account">Account Setting</a></li>
-                <li><a href="<?php echo URL::base(); ?>/customer/billing">Billing information</a></li>
-               
-              </ul>
-            </li>
-            <li><a href="<?php echo URL::base(); ?>/customer/support">Support</a>
-            </li>
-            <li><a href="<?php echo URL::base(); ?>/user/logout">Logout</a> </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+  
+     <?php echo $header; ?>
     <div class="top_line">
       <div class="top_line_in">
         <h1>Step 5: Integrate the referral program into your website</h1>
@@ -88,12 +58,16 @@
 	<?= Form::open('customer/integration'); ?>
     <div class="tabDetails">
     	<div id="tab1" class="tabContents">
-           <textarea name="widget" style="background-color:#FAFAFA; border: medium none; height: 150px;margin: 0; padding: 0;width: 100%;"><script id="invitebox-script" type="text/javascript">(function() {    var ib = document.createElement('script');    ib.type = 'text/javascript';    ib.async = true;    ib.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'invitebox.com/invitation-camp/5587/invitebox.js?key=713a962709b2585eccc840d3cb26142d&jquery='+(typeof(jQuery)=='undefined');    var s = document.getElementsByTagName('script')[0];    s.parentNode.insertBefore(ib, s);})();</script><a id="invitebox-href" href="<?php echo URL::base(); ?>/home/einsert/<?php echo $wid ?>">Refer a friend</a></textarea>
+           <textarea name="widget" style="background-color:#FAFAFA; border: medium none; height: 150px;margin: 0; padding: 0;width: 100%;"><script id="invitebox-script" type="text/javascript">(function() {    var ib = document.createElement('script');    ib.type = 'text/javascript';    ib.async = true;    ib.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'invitebox.com/invitation-camp/5587/invitebox.js?key=713a962709b2585eccc840d3cb26142d&jquery='+(typeof(jQuery)=='undefined');    var s = document.getElementsByTagName('script')[0];    s.parentNode.insertBefore(ib, s);})();</script><a id="invitebox-href" href="<?php echo 'http://softoasistech.com/dev2013/referral/'; ?>importer/index.php?id=<?php echo $wid ?>">Refer a friend</a></textarea>
      
       </div><!-- //tab1 -->
     	<div id="tab2" class="tabContents">
 
-              <textarea name="frame" style="background-color:#FAFAFA; border: medium none; height: 150px;margin: 0; padding: 0;width: 100%;"><script id="invitebox-script" type="text/javascript">(function() {    var ib = document.createElement('script');    ib.type = 'text/javascript';    ib.async = true;    ib.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'invitebox.com/invitation-camp/5587/invitebox.js?key=713a962709b2585eccc840d3cb26142d&jquery='+(typeof(jQuery)=='undefined');    var s = document.getElementsByTagName('script')[0];    s.parentNode.insertBefore(ib, s);})();</script><a id="invitebox-href" href="<?php echo URL::base(); ?>/home/einsert/<?php echo $wid ?>">Refer a friend</a></textarea>
+              <textarea name="frame" style="background-color:#FAFAFA; border: medium none; height: 150px;margin: 0; padding: 0;width: 100%;">
+			   <iframe allowtransparency="true" frameborder="0" scrolling="no" width="800px" height="650px" src="http://softoasistech.com/dev2013/referral/importer/index.php?id=<?php echo $wid ;?>">
+               </iframe>
+
+			  </textarea>
         </div><!-- //tab2 -->
    
   
@@ -116,31 +90,7 @@
     </div>
 	
    
-<div class="foter">
-      <div class="foter_in">
-        <div class="foter_content">
-          <div class="foter_content_in">
-            <ul>
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/about">About US </a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/quality"> Q&A </a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/tou"> Terms of Use</a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/privacypolicy"> Privacy Policy</a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/contact"> Contact US</a></li>
-             
-            </ul>
-            <div class="socil_icon">
-			<?php echo Html::image('public/image/social_icon.png', array('alt'=>''));  ?>
-			</div>
-            
-            <p class="copy">Copyright ©2010-2013 Referral. All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php echo $footer; ?>
 	
   </div>
 </div>
