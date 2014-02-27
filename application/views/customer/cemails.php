3ccaedd99014c21bd178f1c0dd369bb644fcc9aa
jyoti <rose4jyoti@gmail.com>
2014-02-27 13:36:03 +0530

    code modification

--- application/views/customer/cemails.php
+++ application/views/customer/cemails.php
@@ -14,48 +14,10 @@ endforeach;
 
 <div class="wrapper_out">
   <div class="wrapper_in">
-    <div class="header_out">
-      <div class="header_in">
-        <div class="logo">
-		<?php echo Html::image('public/image/dummy-logo.png', array('alt'=>''));  ?>
-		
-		
-		</div>
-       
-        <!---ul class="main_nav">
-          <li><a href="#">Campaigns</a> </li>
-          <li> <a href="#">Account </a></li>
-          <li> <a href="#">Support </a></li>
-          <li><a href="#"> Logout </a></li>
-        </ul--->
-         <span class="cus_name"> Welcome &nbsp;<?php echo $customername; ?></span> 
-		 
-        <div class="new_ul">
-          <ul id="menu">
-            <li><a href="#">Campaigns</a>
-			 <ul>
-			 <?php foreach ($campaigns as $temp) : ?>
-				  <li><a href="<?php echo URL::base(); ?>/customer/dashboard/<?php echo $temp['referralProgDetailsID']; ?>"><?php echo $temp['referralProgTitle']; ?></a></li>
-			 <?php endforeach; ?>	
-                <li><a href="<?php echo URL::base(); ?>/customer/start">Add new Campaigns</a></li> 
-              </ul>
-			</li>
-          
-		      <li><a href="#">Account</a>
-              <ul>
-                <li><a href="<?php echo URL::base(); ?>/customer/account">Account Setting</a></li>
-                <li><a href="<?php echo URL::base(); ?>/customer/billing">Billing information</a></li>
-               
-              </ul>
-            </li>
-            <li><a href="<?php echo URL::base(); ?>/customer/support">Support</a>
-            </li>
-			
-            <li><a href="<?php echo URL::base(); ?>/user/logout">Logout</a> </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+  
+    
+	<?php echo $header;?>
+	
     <div class="top_line">
       <div class="top_line_in">
         <h1>
@@ -319,31 +281,7 @@ endforeach;
 </div>
 
 
-<div class="foter">
-      <div class="foter_in">
-        <div class="foter_content">
-          <div class="foter_content_in">
-            <ul>
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/about">About US </a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/quality"> Q&A </a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/tou"> Terms of Use</a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/privacypolicy"> Privacy Policy</a></li>
-              |
-              <li><a class="special" href="<?php echo URL::base(); ?>/front/contact"> Contact US</a></li>
-             
-            </ul>
-            <div class="socil_icon">
-			<?php echo Html::image('public/image/social_icon.png', array('alt'=>''));  ?>
-			</div>
-            
-            <p class="copy">Copyright ©2010-2013 Referral. All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+ <?php echo $footer; ?>
 
   </div>
 </div>
