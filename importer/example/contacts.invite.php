333c6d76770304f5fa922f5f754267ad4d15b9cc
jyoti <rose4jyoti@gmail.com>
2014-03-10 18:35:23 +0530

    Modified code

--- importer/example/contacts.invite.php
+++ importer/example/contacts.invite.php
@@ -4,6 +4,8 @@
 <?php 
 if($_POST){
  if($_POST['stage']=='1'){
+ 
+ 
   //print_r($_POST);
   //die();
 $host="localhost"; // Host name
@@ -13,16 +15,36 @@ $db_name="softoasi_referral"; // Database name
 
 $tbl_name="rpusers"; // Table name
 // Connect to server and select database.
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
+$conn=mysql_connect("$host", "$username", "$password")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 
+
+//////////////////////////
+/////Redundency check//////
+$email=$_POST['email'];
+$sqlfind="SELECT count(userEmail) AS count FROM rpusers WHERE userEmail='$email' ORDER BY userID DESC LIMIT 1 ";
+$queryresults=mysql_query($sqlfind, $conn);
+$row = mysql_fetch_array($queryresults, MYSQL_ASSOC);
+//$lastid=$row['userID'];
+$count=$row['count'];
+//print_r($row);
+//die();
+
+////////////////////////
+
+
 // Insert data into mysql
+//if($count!='0'){
+
  $email=$_POST['email'];
- $sql="INSERT INTO $tbl_name(userEmail,userReferralID)VALUES('$email','$customerid')";
+ $sql="INSERT INTO rpusers(userEmail,userReferralID)VALUES('$email','$customerid')";
  $result=mysql_query($sql);
- 
 // close connection
 mysql_close();
+ 
+//} 
+ 
+ 
  }
 }
 
@@ -33,24 +55,6 @@ mysql_close();
 <?php 
 define("SITE","http://softoasistech.com/dev2013/referral");
 ?>
-
-<!---
-<form action="" id="invite_form" class="center" method="post">
-		<table cellpadding="4px" cellspacing="0" width="100%;">
-			<tr style="background-color:#D9D9D9; overflow:hidden;">
-				<th><input type="checkbox" id="ToggleSelectedAll" checked="checked" title="Toggle Selected"/></th>
-				<th id="NameColumn">Name</th>
-				<th id="EmailColumn">Email</th>
-			</tr><?php foreach($this->contacts as $contact) {?>
-			<tr style="overflow:hidden">
-				<td><input type="checkbox" name="contacts[<?php echo $contact->email; ?>]" value="<?php echo $contact->name; ?>" checked="checked" /></td>
-				<td><span class="Names"><?php echo $contact->name; ?></span></td>
-				<td><?php echo $contact->email; ?></td>
-			</tr><?php } ?>
-		</table>
-		<button type="submit" id="btnContactsForm" value="invite">Send Email (No email is sent)</button>
-	</form>
----->
 	
 <!----------------------------------------------->
 	
@@ -73,10 +77,11 @@ $sqlquery="SELECT * FROM rpusers  ORDER BY userID DESC LIMIT 1 ";
 $queryresult=mysql_query($sqlquery, $conn);
 $row = mysql_fetch_array($queryresult, MYSQL_ASSOC);
 $lastid=$row['userID'];
+$campaignid=$row['userReferralID'];
 //($row);
-
-$sql="TRUNCATE TABLE $tbl_name";
-$result=mysql_query($sql);
+ 
+//$sql="TRUNCATE TABLE $tbl_name";
+//$result=mysql_query($sql);
  
 
 foreach($this->contacts as $contact) {
@@ -85,8 +90,12 @@ foreach($this->contacts as $contact) {
  //$sql="INSERT INTO $tbl_name(name, email)VALUES('$name', '$email' )";
  //$result=mysql_query($sql);
    
-   $sql2="INSERT INTO rp_users_referrals(referredName, referredEmail,referredByUserID)VALUES('$name', '$email' ,'$lastid')";
+   //if($count!='0'){
+   
+   $sql2="INSERT INTO rp_users_referrals(referredName, referredEmail,referredByUserID,campaign_id)VALUES('$name', '$email' ,'$lastid','$campaignid')";
    $result=mysql_query($sql2);
+  
+   //}
  
  } 
 
@@ -101,8 +110,9 @@ mysql_close();
 <script>
 //function newDoc() {
   //window.location.assign("<?php echo SITE;?>/home/index/<?php echo $customerid; ?>"); 
-  
-  window.location.assign("<?php echo SITE;?>/home/index/<?php echo $customerid=$_REQUEST['id']; ?>")
+  var check='<?php echo $count;?>';
+
+  window.location.assign("<?php echo SITE;?>/home/index/<?php echo $customerid=$_REQUEST['id']; ?>/<?php echo $lastid; ?>")
  
  // }
 </script>
