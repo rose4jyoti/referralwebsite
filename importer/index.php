be2388349f80812078e25342e679da377b0b4660
jyoti <rose4jyoti@gmail.com>
2014-03-28 17:20:34 +0530

    code modification

--- importer/index.php
+++ importer/index.php
@@ -1,6 +1,43 @@
 <?php
-//print_r($_REQUEST);
-echo $customerid=$_REQUEST['id'];
+
+$compaign_id= $_REQUEST['id'];
+$fp = fopen('data.txt', 'w');
+fwrite($fp, $compaign_id);
+
+?>
+<?php
+$compaingn_id =$_REQUEST['id'];
+
+$con=mysqli_connect("localhost","softoasi_referal","]N^fwqZ*@7X9","softoasi_referral");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+/*******No. of Impression*******/
+$result = mysqli_query($con, "SELECT `no_impressions`FROM `referralprogs`WHERE `refProgID` = '$compaingn_id' limit 1");
+
+while($row = mysqli_fetch_array($result))
+  {
+  $current_click= $row['no_impressions'];
+  
+  }
+  $current_click = $current_click+1;
+
+mysqli_query($con,"update `referralprogs` set `no_impressions` = $current_click where `refProgID` = '$compaingn_id'");
+
+/*******No. of clicks*******/
+$result2 = mysqli_query($con, "SELECT `no_clicks`FROM `referralprogs`WHERE `refProgID` = '$compaingn_id' limit 1");
+
+while($row = mysqli_fetch_array($result2))
+  {
+   $current_click= $row['no_clicks'];
+  
+  }
+   $current_click = $current_click+1;
+
+ mysqli_query($con,"update `referralprogs` set `no_clicks` = $current_click where `refProgID` = '$compaingn_id'");
 
 include_once 'example/contacts.main.php';
 $handler = new ContactsHandler();
