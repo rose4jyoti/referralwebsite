d23c99185ab3d5d1426e9a07027ad706ad068b60
jyoti <rose4jyoti@gmail.com>
2014-03-28 11:40:35 +0530

    code modification on 28 march

--- application/config/session.php
+++ application/config/session.php
@@ -1,7 +1,19 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+
+/*****Added by vikash to increase the session time*****/
 return array(
 	'cookie' => array(
 		'encrypted' => TRUE,
+	    'lifetime' => 600
 	),
+   
+   /****added by vikash to increase session time****/	
+	'native' => array(
+        'name' => 'session_name',
+        'lifetime' => 600
+    ),
+	 
+	
 );
+
