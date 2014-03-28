0392ab3b05f91b016f1ef1aee99563c192983f94
jyoti <rose4jyoti@gmail.com>
2014-03-28 11:45:13 +0530

    code modification on 28 march

--- importer/example/contacts.page.php
+++ importer/example/contacts.page.php
@@ -1,7 +1,11 @@
 <?php
+//session_start();
+//echo $_SESSION['compaingnid'];
+?>
+
+<?php
 //print_r($_REQUEST);
  $customerid=$_REQUEST['id'];
- //echo $customerid;
 ?>
 
 <!----------Data fetching from database----->
@@ -30,7 +34,7 @@ $sql2 ="SELECT * FROM referralprogdetails WHERE referralProgID='". $customerid."
 $result2 = mysql_query( $sql2, $conn );
 while($row = mysql_fetch_array($result2, MYSQL_ASSOC))
 {
-      $refprogtitle= $row['referralProgTitle'];
+      $refprogdesc= $row['referralProgDescription'];
 }
 
 /*********Referralprogs*************/
@@ -83,36 +87,6 @@ mysql_close($conn);
 <div class="wrapper_out">
   <div class="wrapper_in" style="width:auto;">
      
-	 
-	 <!----
-	 <div class="header_out">
-      <div class="header_in">
-        <div class="logo">
-
-	    <?php //echo Html::image('public/image/dummy-logo.png', array('alt'=>''));  ?>
-		 <?php //echo 'hello';  ?>
-		  <img src="http://softoasistech.com/dev2013/referral/public/image/dummy-logo.png"/>
-		</div>     
-        
-		  <ul class="main_nav">
-          <li><a class="active" href="http://softoasistech.com/dev2013/referral/front/home"> Home</a> </li>
-          <li> <a href="http://softoasistech.com/dev2013/referral/front/hiw">How it works </a></li>
-          <li> <a href="http://softoasistech.com/dev2013/referral/front/price">Pricing </a></li>
-          <li><a href="http://softoasistech.com/dev2013/referral/front/blog"> Blog </a></li>
-
-        </ul>
-    
-	</div>
-    </div>
-	
-	<div class="top_line">
-      <div class="top_line_in">
-        <h1> </h1>
-      </div>
-		
-    </div>
-          ------->
-
 	
 <!-------------------start contents----------------->
 
@@ -120,12 +94,7 @@ mysql_close($conn);
    <div class="img1" style="margin: 0px">
                 	<div class="img_in">
                                      <div style="padding: 0 0 0 0; min-height: 401px;" class="left">
-                            <!---p>
-                            <b>INVITE YOUR FRIENDS</b> to enter this
-                            contest for a chance to win a FREE 
-                            Samssung Galaxy Note.
-                            </p--->
-
+                           
 			
                          <?php  if(isset($img1)){ ?>
   
@@ -141,84 +110,17 @@ mysql_close($conn);
                             <div class="right">
                             <p>
 								<b style="font-weight:bold;text-transform: capitalize;"> 
-								        <?php echo substr($refprogtitle, '0', '30').'..';?>                     	
+								        <?php //echo substr($refprogdesc, '0', '30').'..';?>                     	
+								        <?php echo $refprogdesc;?>                     	
 							    </b>
-							<!---
-                            STAR SHARING NOW for a chance to win
-                            your<b> FREE Samsung Galaxy Note.</b>
-							---->
+						
 	                       </p>
-	
-	
-	                    
+				
 						
-						
-						<!------
-						 <?php //echo Form::open ( 'http://softoasistech.com/dev2013/referral/home/index', array( 'method' => 'post', 'name' =>'reg_frm', 'id' => 'reg_frm', 'enctype' => 'multipart/form-data') ); ?>
-                            <table height="229" class="table">
-							
-							<tbody><tr>
-							  <td><h1>Login to your webmail account below <b>(Hotmail,Gmail,Yahoo,AOL)</b>
-							  and discover how much money you colud receive!</h1>
-							  </td>
-                              </tr>
-                              <tr> 
-							
-                              </tr><tr>
-							  <td>Email</td>
-                              </tr>
-                              <tr> 
-                                <td><input type="text" class="in_box" name="email"></td>
-                              </tr>
-                              <tr>
-                                <td>Your password</td>
-                              </tr>
-                              <tr>
-                                  <td><input type="password" class="in_box" name="pass"><a href="#">
-								
-								<input name="id" value="<?php //echo $id;?>" type="hidden" />
-							
-								
-								<button type="submit" style="border:none; background:none;">
-								<?php //echo Html::image('public/image/next_but.jpg', array('alt'=>''));  ?>
-                                </button>
-								
-								</a></td>
-                              </tr>
-                              <tr>
-                                <td>
-								<br>
-								<span style="font:11px Verdana,Geneva,sans-serif;">
-								Already have a Participant number? <a href="#">Click here</a></span><a href="#">
-								</a>
-								</td>
-                              </tr>
-                            </tbody>
-							
-							  <?php //echo Form::close(); ?>	
-							</table>
-	                       ----->
 
 
 <?php require_once $this->include_form; ?>
-		
-<!----
-<?php if ($this->display_menu) {?>
 
-
-<br/>
-<ul id="menu">
-<?php foreach ($this->contacts_classes as $k => $v) {?>
-	<li><a href="<?php echo "{$this->base_url}contacts_option=$k"; ?>"><?php //echo $v->ClassName . ($v->ExternalAuth ? "<br/>(External Authentication)" : ""); ?></a></li>
-	<?php } ?>
-</ul>
-	<?php } ?>
-<div class="clear">&nbsp;</div>
-
----->	
-	
-	
-	
 	 
 	         <div class="lock">
 							<img class="bbb_n" alt="" src="/dev2013/referral/public/image/lock.jpg">						
@@ -241,83 +143,9 @@ mysql_close($conn);
 		  
 </div>
 
-
-
-
-
-
-
-<!-------------------end contents----------------->
-
-<!----
-<div class="foter" style=>
-      <div class="foter_in"  style="float: left; margin: 32px 0px 0px;">
-        <div class="foter_content">
-          <div class="foter_content_in">
-            <ul>
-              <li>About US </li>
-              |
-              <li> Q&A </li>
-              |
-              <li> Terms of Use</li>
-              |
-              <li>Privacy Policy</li>
-              |
-              <li>Contact US </li>
-             
-            </ul>
-            <div class="socil_icon">
-			<?php //echo Html::image('public/image/social_icon.png', array('alt'=>''));  ?>
-			<img alt="" src="http://softoasistech.com/dev2013/referral/public/image/social_icon.png">
-			
-			</div>
-            
-            <p class="copy">Copyright ©2010-2013 Referral. All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </div>
----->
-	
-	
-	
    </div>
   </div>
   
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-<!------------------------------------------------------------->		
-		
-<!----
-<div class="content">
-<div id="<?php echo $this->display_menu ? "importform" : "inviteform"; ?>" class="center">
-<h1>Address Book<br />
-Importer Demo</h1>
-<h3 class="center"><?php echo isset($this->current_class) ? "{$this->current_class->ClassName} Contacts Importer" : ""; ?>
-</h3>
-<br />
-<?php require_once $this->include_form; ?></div>
-<?php if ($this->display_menu) {?>
-<br/>
-<ul id="menu">
-<?php foreach ($this->contacts_classes as $k => $v) {?>
-	<li><a href="<?php echo "{$this->base_url}contacts_option=$k"; ?>"><?php echo $v->ClassName . ($v->ExternalAuth ? "<br/>(External Authentication)" : ""); ?></a></li>
-	<?php } ?>
-</ul>
-	<?php } ?>
-<div class="clear">&nbsp;</div>
-</div>
----->
 
 <!---------------------------------------------------------->
 
