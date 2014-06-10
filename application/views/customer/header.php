2bab15e2ba2f9ca07ef227d04ef79845abe243d0
jyoti <rose4jyoti@gmail.com>
2014-06-10 19:25:59 +0530

    modification

--- application/views/customer/header.php
+++ application/views/customer/header.php
@@ -20,28 +20,28 @@
         
         <div class="new_ul">
           <ul id="menu">
-            <li><a href="<?php echo URL::base(); ?>/customer/campaign">Campaigns</a>
+            <li><a href="<?php echo URL::base(); ?>customer/campaign">Campaigns</a>
 			<ul>
 			 <?php foreach ($campaigns as $temp) : ?>
-              <li><a href="<?php echo URL::base(); ?>/customer/dashboard/<?php echo $temp['referralProgDetailsID']; ?>"><?php echo $temp['referralProgTitle']; ?></a></li>	
+              <li><a href="<?php echo URL::base(); ?>customer/dashboard/<?php echo $temp['referralProgDetailsID']; ?>"><?php echo $temp['referralProgTitle']; ?></a></li>	
 			 <?php endforeach; ?>	
 				
-                <li><a href="<?php echo URL::base(); ?>/customer/start">Add new Campaigns</a></li> 
+                <li><a href="<?php echo URL::base(); ?>customer/start">Add new Campaigns</a></li> 
               </ul>
 
 			</li>
 			
 		  <li><a href="#">Account</a>
               <ul>
-                <li><a href="<?php echo URL::base(); ?>/customer/account">Account Setting</a></li>
-                <li><a href="<?php echo URL::base(); ?>/customer/billing">Billing information</a></li>
+                <li><a href="<?php echo URL::base(); ?>customer/account">Account Setting</a></li>
+                <li><a href="<?php echo URL::base(); ?>customer/billing">Billing information</a></li>
                
               </ul>
             </li>
-            <li><a href="<?php echo URL::base(); ?>/customer/support">Support</a>
+            <li><a href="<?php echo URL::base(); ?>customer/support">Support</a>
             </li>
 			
-            <li><a href="<?php echo URL::base(); ?>/user/logout">Logout</a> </li>
+            <li><a href="<?php echo URL::base(); ?>user/logout">Logout</a> </li>
           </ul>
         </div>
       </div>
